ffd827baf9382d83905a0ae50c3cddbaca288e0d
yfern328 <yfernan2@u.rochester.edu>
2017-06-27 14:53:57 -0400

    Done. But with erbs instead of string interpolation

--- app.rb
+++ app.rb
@@ -2,17 +2,35 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  # This is a sample static route.
+  get '/' do
+    erb :index
+  end
+
+  #This is a sample static route.
   get '/hello' do
     "Hello World!"
+    #erb :hello
   end
 
   # This is a sample dynamic route.
   get "/hello/:name" do
     @user_name = params[:name]
-    "Hello #{@user_name}!"
+    erb :hello
+    #"Hello #{@user_name}!"
   end
 
   # Code your final two routes here:
+  get "/goodbye/:name" do
+    @user_name = params[:name]
+    erb :goodbye
+    #"Goodbye, #{@user_name}."
+  end
+
+  get "/multiply/:num1/:num2" do
+    @num1 = params[:num1].to_i
+    @num2 = params[:num2].to_i
+    @result = (@num1 * @num2).to_s
+    erb :multiply
+  end
 
 end
